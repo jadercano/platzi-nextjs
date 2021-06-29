621aca9857e4added107bb024e73ab870c7eb884
Jader Cano <jader.cano@contractors.accruent.com>
2021-06-29 13:48:51 -0500

    Added Product Summary Component.

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,25 +1,30 @@
+import Layout from "@components/Layout/Layout";
+import ProductSummary from "@components/ProductSummary/ProductSummary";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const ProductPage = () => {
-  const {
-    query: { id },
-  } = useRouter();
-
+  const { query } = useRouter();
   const [product, setProduct] = useState<TProduct>();
 
   useEffect(() => {
-    window
-      .fetch(`/api/avo/${id}`)
-      .then((response) => response.json())
-      .then((data) => setProduct(data));
-  }, []);
+    if (query.id) {
+      window
+        .fetch(`/api/avo/${query.id}`)
+        .then((response) => response.json())
+        .then((data: TProduct) => setProduct(data));
+    }
+  }, [query.id]);
 
   if (!product) {
     return <h1>Loading...</h1>;
   }
 
-  return <h1>Page details for product {product?.name}</h1>;
+  return (
+    <Layout>
+      {product == null ? null : <ProductSummary product={product} />}
+    </Layout>
+  );
 };
 
 export default ProductPage;
