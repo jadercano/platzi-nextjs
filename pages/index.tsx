4a484c7b77917a7238c6836cea58b00456d675b2
Jader Cano <jader.cano@contractors.accruent.com>
2021-06-29 16:02:09 -0500

    Changed the HomePage Component to use Server Side Rendering.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import KawaiiHeader from "@components/KawaiiHeader/KawaiiHeader";
 import Layout from "@components/Layout/Layout";
 import ProductList from "@components/ProductList/ProductList";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([]);
+export const getServerSideProps = async () => {
+  const {
+    data: { data: productList },
+  } = await axios.get<TAPIAvoResponse>("http://localhost:3000/api/avo");
+  return {
+    props: {
+      productList,
+    },
+  };
+};
 
-  useEffect(() => {
-    window
-      .fetch("/api/avo")
-      .then((response) => response.json())
-      .then(({ data }) => setProductList(data));
-  }, []);
-
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
